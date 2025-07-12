4582b8f68b924cb7e031f3bf68b6acdfb6b6134d
Rishabh-devloper <rishabhmishra143a@gmail.com>
2025-07-12 10:37:09 +0530

    Upload Page

---
+++ app/(logged-in)/upload/page.tsx
@@ -0,0 +1,20 @@
+import BgGradient from "@/components/common/bg-gradient";
+import UploadForm from "@/components/upload/upload-form";
+
+import UploadHeder from "@/components/upload/upload-header";
+
+
+
+export default function Upload() {
+    return (
+        <section className="min-h-screen">
+            <BgGradient />
+            <div className="mx-auto max-w-7xl px-6 py-24 sm:py-32 lg:px-8  ">
+               <div className="flex flex-col items-center justify-center gap-6 text-center ">
+                <UploadHeder />
+                <UploadForm/>
+               </div>
+            </div>
+        </section>
+    )
+}
---
+++ components/upload/upload-form.tsx
@@ -0,0 +1,7 @@
+export default function UploadForm(){
+    return(
+        <div>
+            uplaoddddd
+        </div>
+    )
+}
